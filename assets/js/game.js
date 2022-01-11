2f3e345d74df22a0e8a1132d1bee49b2b7776b0e
Your Name <briancasillas67@yahoo.com>
2022-01-11 13:21:34 -0800

    work

---
+++ assets/js/game.js
@@ -0,0 +1,8 @@
+var playername = window.prompt("What is your robot's name?");
+console.log(playername);
+console.log(10+10)
+console.log("our robots name is " + playername);
+function fight() {
+    window.alert("The fight has begun!");
+  }
+  //fight();
